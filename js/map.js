46d4a3443175b093c2b9f9e08c639a67c52557cb
YevhDr <evgesha.drozdova@gmail.com>
2019-07-23 00:03:23 +0300

    init

--- js/map.js
+++ js/map.js
@@ -6,72 +6,147 @@
 
 var map = L.map('map').setView([49.35, 29.51], 6);
 
+map.zoomControl.setPosition('bottomright');
 
-// var CartoDB_Positron = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-//     subdomains: 'abcd',
-//     minZoom: 6,
-//     maxZoom: 18
-//
-// }).addTo(map);
+//https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png  чорна
+///https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png
 
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
+
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
     attribution: 'Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL',
     minZoom: 6,
-    maxZoom: 18
+    maxZoom: 10
 }).addTo(map);
 
 
+
+//Шар з поточними виборами
+var additionalLayer = new L.LayerGroup();
+
+
 function getColor(d) {
-    return d === 'Політична Партія Біла Церква разом' ? 'red' :
-        d === 'політична партія Всеукраїнське об’єднання Батьківщина' ? '#e28671' :
-            d === 'політична партія Всеукраїнське об’єднання Свобода'  ? 'red' :
-                d === 'Політична Партія ГОЛОС'  ? 'orange' :
-                    d === 'Політична партія Європейська Солідарність'   ? '#ca4c6e' :
-                        d === 'політична партія Єдиний Центр'   ? 'red' :
-                            d === 'Політична партія Об’єднання САМОПОМІЧ'   ? '#4fe6fc' :
-                                d === 'Політична партія ОПОЗИЦІЙНА ПЛАТФОРМА – ЗА ЖИТТЯ'  ? 'blue' :
-                                    d === 'Політична партія ОПОЗИЦІЙНИЙ БЛОК'  ? 'lightblue' :
-                                        d === 'ПОЛІТИЧНА ПАРТІЯ СЛУГА НАРОДУ' ? "#7ade24":
-                                            d === 'Самовисування' ? "#fae41a":
-                                               'lightgrey';
+    return d === 'Політична Партія Біла Церква разом' ? '#fb9a99' :
+        d === 'політична партія Всеукраїнське об’єднання Батьківщина' ? '#fdae61' :
+            d === 'політична партія Всеукраїнське об’єднання Свобода'  ? '#fb9a99' :
+                d === 'Політична Партія ГОЛОС'  ? '#d53e4f' :
+                    d === 'Політична партія Європейська Солідарність'   ? '#cab2d6' :
+                        d === 'політична партія Єдиний Центр'   ? '#fb9a99' :
+                            d === 'Політична партія Об’єднання САМОПОМІЧ'   ? '#fb9a99' :
+                                d === 'Політична партія ОПОЗИЦІЙНА ПЛАТФОРМА – ЗА ЖИТТЯ'  ? '#66c2a5' :
+                                    d === 'Політична партія ОПОЗИЦІЙНИЙ БЛОК'  ? '#a6cee3' :
+                                        d === 'ПОЛІТИЧНА ПАРТІЯ СЛУГА НАРОДУ' ? "#a6d96a":
+                                            d === 'Самовисування' ? "#ffffbf":
+                                               'white';
 }
+
 
 function style(feature) {
     return {
         fillColor: getColor(feature.properties.winners_party),
         weight: 1,
         opacity: 1,
-        color: 'black',
+        color: 'grey',
         //dashArray: '3',
-        fillOpacity: 0.7
+        fillOpacity: 1
     };
 }
 
-
-function whenClicked(d) {
-    // e = event
-    console.log(d.target.feature.properties.winners_party);
-    // You can make your ajax call declaration here
-    //$.ajax(...
-}
-
 function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
-        layer.bindPopup("<b>ВИБОРЧИЙ ОКРУГ № " + feature.properties.id + "<br>" + feature.properties.winners_name + "</b> <br>" + feature.properties.winners_info);
-
+    layer.bindPopup("<b>ВИБОРЧИЙ ОКРУГ № " + feature.properties.id + "<br>" + feature.properties.winners_name + "</b> <br>" + feature.properties.winners_info);
 }
 
-// var geojsonLayer = new L.GeoJSON.AJAX("data/okrugi_shp.geojson");
 var geojsonLayer = new L.GeoJSON.AJAX("data/all_tvo_with_result.geojson", { style: style,  onEachFeature: onEachFeature} );
 
-geojsonLayer.addTo(map);
+var legend = L.control({position: 'bottomleft'});
+
+legend.onAdd = function () {
+    var div = L.DomUtil.create("div", "legend");
+    div.innerHTML += "<h4>Кольорами позначено:</h4>";
+    div.innerHTML += '<i style="background: #a6d96a"></i><span>Слуга народу</span><br>';
+    div.innerHTML += '<i style="background: #ffffbf"></i><span>Самовисуванці</span><br>';
+    div.innerHTML += '<i style="background: #66c2a5"></i><span>Оппозиційна платформа - За життя</span><br>';
+    div.innerHTML += '<i style="background: #fdae61"></i><span>Батьківщина</span><br>';
+    div.innerHTML += '<i style="background: #cab2d6"></i><span>Європейська солідарність</span><br>';
+    div.innerHTML += '<i style="background: #d53e4f"></i><span>Голос</span><br>';
+    div.innerHTML += '<i style="background: #a6cee3"></i><span>Оппозиційний блок</span><br>';
+    div.innerHTML += '<i style="background: #fb9a99"></i><span>Інші</span><br>';
+
+
+    return div;
+};
 
 
 
+legend.addTo(map);
+geojsonLayer.addTo(additionalLayer);
+additionalLayer.addTo(map);
 
 
 
+/*  Шар з тими, хто пройшов/пролетів*/
+
+var old_rada = new L.LayerGroup();
+
+function getColor_oldrada(d) {
+    return d === 'true' ? '#ffffbf' :
+                d === 'false' ? '#a6d96a' :
+                    'white';
+}
 
 
+function style_oldrada(feature) {
+    return {
+        fillColor: getColor_oldrada(feature.properties.deputes_14_exit),
+        weight: 0.5,
+        opacity: 1,
+        color: 'grey',
+        //dashArray: '3',
+        fillOpacity: 1
+    };
+}
 
+function onEachFeature_oldrada(feature, layer) {
+    layer.bindPopup("<b>ВИБОРЧИЙ ОКРУГ № " + feature.properties.id + "<br>" + feature.properties.deputes_14_name + "</b>");
+}
+
+
+//депутати 2014 року
+var geojsonLayer_oldrada = new L.GeoJSON.AJAX("data/all_tvo_2014.geojson", { style: style_oldrada, onEachFeature: onEachFeature_oldrada} );
+geojsonLayer_oldrada.addTo(old_rada);
+
+document.getElementById('winners').style.background = "#C5E1A5";
+
+
+/* Переключення між кнопками */
+
+
+const winners = document.getElementById('winners');
+winners.addEventListener('click', function(e) {
+    document.getElementById('winners').style.background = "#C5E1A5";
+    // document.getElementById('winners').style.color = "white";
+    document.getElementById('losers').style.background = "linear-gradient(to bottom, #ffffff 5%, #f6f6f6 100%)";
+    document.getElementById('losers').style.color = " #666666";
+
+
+    additionalLayer.remove(geojsonLayer);
+    geojsonLayer.addTo(additionalLayer);
+    additionalLayer.addTo(map);
+    // additionalLayer.addTo(map);
+    old_rada.remove(geojsonLayer_oldrada);
+
+});
+
+const losers = document.getElementById('losers');
+losers.addEventListener('click', function(e) {
+    document.getElementById('losers').style.background = "#C5E1A5";
+    // document.getElementById('losers').style.color = "white";
+    document.getElementById('winners').style.background = "linear-gradient(to bottom, #ffffff 5%, #f6f6f6 100%)";
+    document.getElementById('winners').style.color = " #666666";
+    map.removeControl(legend);
+
+
+    additionalLayer.remove(geojsonLayer);
+    geojsonLayer_oldrada.addTo(old_rada);
+    old_rada.addTo(map);
+
+});
