855f406cca555ea1f01a6ae74477cb16345535b9
YevhDr <evgesha.drozdova@gmail.com>
2019-07-23 02:41:18 +0300

    init

--- js/map.js
+++ js/map.js
@@ -143,8 +143,8 @@ var legend_oldrada = L.control({position: 'bottomleft'});
 legend_oldrada.onAdd = function () {
     var div = L.DomUtil.create("div", "legend_oldrada");
     div.innerHTML += "<h4>Кольорами позначено:</h4>";
-    div.innerHTML += '<i style="background: #a6d96a"></i><span>Втримались</span><br>';
-    div.innerHTML += '<i style="background: #ffffbf"></i><span>Не втримались</span><br>';
+    div.innerHTML += '<i style="background: #a6d96a"></i><span>Депутат 8 скликання</span><br>';
+    div.innerHTML += '<i style="background: #ffffbf"></i><span>Нове обличчя</span><br>';
     return div;
 };
 
